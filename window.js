cb69a392419ad112e055b019dc0a8e1a826349cc
ITCOM <159421917+Sysyz-itcom@users.noreply.github.com>
2024-08-19 20:27:13 +0800

    Update window.js

--- window.js
+++ window.js
@@ -8,7 +8,7 @@ function showrth(){
 			closeBtn: 0,
 			shadeClose: true,
 			time: 2000,
-			content: '<div style="padding:20px;"><h2 style="color: #01b3eb;">特别提示</h2><h4>上游服务出现问题</h4><h4>解析接口可能出现异常。</h4><h4>我们正在尝试修复该问题！</h4></div>'
+			content: '<div style="padding:20px;"><h2 style="color: #01b3eb;">特别提示</h2><h4>上游服务出现问题</h4><h4>当前页面服务可能出现异常</h4><h4>我们正在尝试修复该问题！</h4></div>'
 		})
 	})
 }
